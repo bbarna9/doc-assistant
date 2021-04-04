22fee3320b598c4aeb69b8ce8d6e56c58d4c2d7d
KDani-99 <porsekin@gmail.com>
2021-04-04 13:28:36 +0200

    Add StreetValidation

---
+++ DocAssistant_Shared/Attributes/Validation/StreetValidationAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DocAssistant_Common.Attributes
+{
+    public class StreetValidationAttribute : ModelValidationAttribute
+    {
+        public StreetValidationAttribute(int minLength, int maxLength) : base(minLength,maxLength) {}
+        
+        public override bool IsValid([NotNull] object value)
+        {
+            return base.IsValid(value);
+        }
+    }
+}
