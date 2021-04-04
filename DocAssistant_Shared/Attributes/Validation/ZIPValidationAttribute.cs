097f5ebe07ab0ba4545875b83d8511f9c43cccc0
KDani-99 <porsekin@gmail.com>
2021-04-04 13:28:42 +0200

    Add ZIPValidation

---
+++ DocAssistant_Shared/Attributes/Validation/ZIPValidationAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DocAssistant_Common.Attributes
+{
+    public class ZIPValidationAttribute : ModelValidationAttribute
+    {
+        public ZIPValidationAttribute(int minLength, int maxLength) : base(minLength,maxLength) {}
+        
+        public override bool IsValid([NotNull] object value)
+        {
+            var zip = (string) value;
+
+            return zip.Length <= this.MaxLength && zip.Length >= this.MinLength;
+        }
+    }
+}
