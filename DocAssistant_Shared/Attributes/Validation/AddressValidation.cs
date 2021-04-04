5b6f42e725516dbb6636cae6b7bee1c076dbfb8c
KDani-99 <porsekin@gmail.com>
2021-04-04 13:26:54 +0200

    Add AddressValidation

---
+++ DocAssistant_Shared/Attributes/Validation/AddressValidation.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using DocAssistant_Common.Models;
+using DocAssistant_Common.Utils;
+
+namespace DocAssistant_Common.Attributes
+{
+    public class AddressValidation : ModelValidationAttribute
+    {
+        public override bool IsValid([NotNull] object value)
+        {
+            var address = (Address) value;
+
+            if (!Validation.ValidateProperty("Country", address, out var countryValidationErrors))
+            {
+                this.CreateValidationErrorsListIfNotExists();
+                this.ValidationErrors.Add(("Country",countryValidationErrors));
+            }
+
+            if (!Validation.ValidateProperty("State", address, out var stateValidationErrors))
+            {
+                this.CreateValidationErrorsListIfNotExists();
+                this.ValidationErrors.Add(("State",stateValidationErrors));
+            }
+            
+            if (!Validation.ValidateProperty("City", address, out var cityValidationErrors))
+            {
+                this.CreateValidationErrorsListIfNotExists();
+                this.ValidationErrors.Add(("City",cityValidationErrors));
+            }
+            
+            if (!Validation.ValidateProperty("Street", address, out var streetValidationErrors))
+            {
+                this.CreateValidationErrorsListIfNotExists();
+                this.ValidationErrors.Add(("Street",streetValidationErrors));
+            }
+
+            if (!Validation.ValidateProperty("Street", address, out streetValidationErrors))
+            {
+                this.CreateValidationErrorsListIfNotExists();
+                this.ValidationErrors.Add(("Street",streetValidationErrors));
+            }
+            
+            if (!Validation.ValidateProperty("ZIP", address, out var zipValidationErrors))
+            {
+                this.CreateValidationErrorsListIfNotExists();
+                this.ValidationErrors.Add(("ZIP",zipValidationErrors));
+            }
+
+            return
+                (countryValidationErrors is not null) ||
+                (stateValidationErrors is not null) ||
+                (cityValidationErrors is not null) ||
+                (streetValidationErrors is not null) ||
+                (zipValidationErrors is not null);
+        }
+    }
+}
