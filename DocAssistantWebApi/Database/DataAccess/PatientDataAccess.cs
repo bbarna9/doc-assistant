751a73393d7bcd89fac595893cd577696ef31c53
KDani-99 <porsekin@gmail.com>
2021-04-04 21:12:12 +0200

    Refactor PatientDataAccess methods

--- DocAssistantWebApi/Database/DataAccess/PatientDataAccess.cs
+++ DocAssistantWebApi/Database/DataAccess/PatientDataAccess.cs
@@ -18,10 +18,10 @@ namespace DocAssistantWebApi.Database.DataAccess
         public async Task<Patient> Get(Patient entity)
         {
             Patient patient = null;
-            using (var ctx = new SQLiteDatabaseContext(this.connectionString))
-            {
-                patient = (Patient) await ctx.Patients.FindAsync(entity);
-            }
+            
+            await using var ctx = new SQLiteDatabaseContext(this.connectionString);
+            
+            patient = (Patient) await ctx.Patients.FindAsync(entity);
 
             return patient;
         }
@@ -29,19 +29,27 @@ namespace DocAssistantWebApi.Database.DataAccess
         public async Task<Patient> GetById(long id)
         {
             Patient patient = null;
-            using (var ctx = new SQLiteDatabaseContext(this.connectionString))
-            {
-                patient = (Patient) await ctx.Patients.FindAsync(new Patient{ Id = id});
-            }
+            
+            await using var ctx = new SQLiteDatabaseContext(this.connectionString);
+            
+            patient = (Patient) await ctx.Patients.FindAsync(new Patient{ Id = id});
 
             return patient;
         }
 
         public async Task Update(Patient entity)
         {
-          //  using (var ctx = new SQLiteDatabaseContext(this.connectionString))
-              //  await ctx.Patients.Update(entity);
-              throw new NotImplementedException();
+            await using var ctx = new SQLiteDatabaseContext(this.connectionString);
+            
+            var patientToUpdate = await ctx.Patients.FirstOrDefaultAsync();
+            if (patientToUpdate != null)
+            {
+                patientToUpdate.FirstName = entity.FirstName;
+                patientToUpdate.LastName = entity.LastName;
+
+                    
+                await ctx.SaveChangesAsync();
+            }
         }
         
         public async Task Save(Patient entity)
