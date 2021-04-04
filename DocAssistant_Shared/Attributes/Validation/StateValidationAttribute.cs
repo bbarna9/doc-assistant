87fc391af8b9168f956d37ffab947c35cf732292
KDani-99 <porsekin@gmail.com>
2021-04-04 13:28:30 +0200

    Add StateValidation

---
+++ DocAssistant_Shared/Attributes/Validation/StateValidationAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DocAssistant_Common.Attributes
+{
+    public class StateValidationAttribute : ModelValidationAttribute
+    {
+        
+        public StateValidationAttribute(int minLength, int maxLength, char [] invalidCharacters) : base(minLength,maxLength,invalidCharacters) {}
+        
+        public override bool IsValid([NotNull] object value)
+        {
+            var state = (string) value;
+
+            if (state.Length < MinLength || state.Length > MaxLength) return false;
+            
+            foreach (var invalidChar in InvalidCharacters)
+                if (state.Contains(invalidChar)) return false;
+
+            return true;
+        }
+    }
+}
