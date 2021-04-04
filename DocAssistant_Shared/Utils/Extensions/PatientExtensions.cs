a9e91fd5f508d4db8341723db30aa13145e29c37
KDani-99 <porsekin@gmail.com>
2021-04-04 13:29:44 +0200

    Add PatientExtensions extension class

---
+++ DocAssistant_Shared/Utils/Extensions/PatientExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using DocAssistant_Common.Models;
+
+namespace DocAssistant_Common.Utils.Extensions
+{
+    public static class PatientExtensions
+    {
+
+        public static bool ValidateData(this Patient patient, string propertyName, out List<string> errorMessages) =>
+            Validation.ValidateProperty(propertyName, patient, out errorMessages);
+
+    }
+}
