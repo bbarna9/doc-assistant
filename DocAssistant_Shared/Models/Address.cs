c3f2436c8abc6f4f7e47a2bb7f627631b251c2a0
KDani-99 <porsekin@gmail.com>
2021-04-04 13:28:55 +0200

    Add Models

---
+++ DocAssistant_Shared/Models/Address.cs
@@ -0,0 +1,27 @@
+﻿using DocAssistant_Common.Attributes;
+
+namespace DocAssistant_Common.Models
+{
+    public struct Address
+    {
+        [CountryValidation] 
+        public string Country { get; set; }
+        [StateValidation(minLength: 1, maxLength: 255, invalidCharacters: new char[] {'~','!','@','#','$','%','^','&','*','(',')','_','=','+','{','}',';','\"','<','>','?','\\','/','.','|'})] 
+        public string State { get; set; }
+        [CityValidation(minLength:1, maxLength: 255, invalidCharacters: new char[] {'~','!','@','#','$','%','^','&','*','(',')','_','=','+','{','}',';','\"','<','>','?','\\','/','.','|'} )] 
+        public string City { get; set; }
+        [StreetValidation(minLength: 1, maxLength: 255)] 
+        public string Street { get; set; }
+        [ZIPValidation(minLength: 1, maxLength: 10)] 
+        public string ZIP { get; set; }
+
+        public Address(string country, string state, string city, string street, string zip)
+        {
+            this.Country = country;
+            this.State = state;
+            this.City = city;
+            this.Street = street;
+            this.ZIP = zip;
+        }
+    }
+}
---
+++ DocAssistant_Shared/Models/Patient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using DocAssistant_Common.Attributes;
+
+namespace DocAssistant_Common.Models
+{
+    public sealed class Patient
+    {
+        public long Id { get; set; }
+        
+        [NameValidation(minLength:1,maxLength:50)] public string FirstName { get; set; }
+        [NameValidation(minLength:1,maxLength:50)] public string LastName { get; set; }
+        [AddressValidation] public Address Address { get; set; }
+        [SSNValidation] public StringBuilder SSN { get; set; }
+        
+        public DateTime DateOfBirth { get; set; }
+
+        public Patient()
+        {
+           
+        }
+        
+    }
+}
