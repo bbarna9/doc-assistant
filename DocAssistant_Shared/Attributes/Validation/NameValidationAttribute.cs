f0cc7aeec277f05f5c6d4de00ace76786187e402
KDani-99 <porsekin@gmail.com>
2021-04-04 13:28:16 +0200

    Add NameValidation

---
+++ DocAssistant_Shared/Attributes/Validation/NameValidationAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DocAssistant_Common.Attributes
+{
+    public class NameValidationAttribute : ValidationAttribute
+    {
+        public int MinLength { get; set; }
+        public int MaxLength { get; set; }
+        public NameValidationAttribute(int minLength, int maxLength)
+        {
+            this.MinLength = minLength;
+            this.MaxLength = maxLength;
+        }
+        public override bool IsValid([NotNull] object value)
+        {
+            return base.IsValid(value);
+        }
+    }
+}
