cbd0b998852bdfd80026694ac5a629dbc90b4441
KDani-99 <porsekin@gmail.com>
2021-04-04 13:27:00 +0200

    Add CityValidation

---
+++ DocAssistant_Shared/Attributes/Validation/CityValidationAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+
+namespace DocAssistant_Common.Attributes
+{
+    public class CityValidationAttribute : ModelValidationAttribute
+    {
+        public CityValidationAttribute(int minLength, int maxLength, char [] invalidCharacters) : base(minLength,maxLength,invalidCharacters) {}
+        
+        public override bool IsValid([NotNull] object value)
+        {
+            var city = (string) value;
+
+            if (city.Length < MinLength || city.Length > MaxLength) return false;
+
+            foreach (var invalidChar in InvalidCharacters)
+                if (city.Contains(invalidChar)) return false;
+            
+            return true;
+        }
+    }
+}
