e3fcee77821eee5de398785ff7dcec27bef0abf6
KDani-99 <porsekin@gmail.com>
2021-04-04 13:30:44 +0200

    Add default controllers

---
+++ DocAssistantWebApi/Controllers/AuthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DocAssistantWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController
+    {
+        [HttpPost]
+        public ActionResult<> Auth()
+        {
+            
+        }
+    }
+}
---
+++ DocAssistantWebApi/Controllers/PatientController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DocAssistant_Common.Models;
+
+namespace DocAssistantWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/patient")]
+    public class PatientController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<Patient> Get()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult<Patient> Save()
+        {
+            return Ok();
+        }
+    }
+}
