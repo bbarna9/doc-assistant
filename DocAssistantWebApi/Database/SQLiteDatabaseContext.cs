03c04b45949f21d668dd15330cfcd70ddc04a71e
KDani-99 <porsekin@gmail.com>
2021-04-04 13:31:50 +0200

    Add SQLite Entity Framework database context

---
+++ DocAssistantWebApi/Database/SQLiteDatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using DocAssistantWebApi.Database.DbModels;
+using Microsoft.EntityFrameworkCore;
+using Patient = DocAssistant_Common.Models.Patient;
+
+namespace DocAssistantWebApi.Database
+{
+    public sealed class SQLiteDatabaseContext : DbContext
+    {
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+
+        private readonly string connectionString; // @"Data Source=" + Directory.GetCurrentDirectory() + "/healthcare.db"
+        
+        public SQLiteDatabaseContext(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite(this.connectionString); // TODO: use config file connection string
+    }
+}
