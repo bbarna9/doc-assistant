14c173481ef0a99836a7ab5be6d73784b9acb0fd
KDani-99 <porsekin@gmail.com>
2021-04-04 13:31:13 +0200

    Add PatientDataAccess

---
+++ DocAssistantWebApi/Database/DataAccess/PatientDataAccess.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+using System.Threading.Tasks;
+using DocAssistantWebApi.Database.DbModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace DocAssistantWebApi.Database.DataAccess
+{
+    public class PatientDataAccess : IDataAccess<Patient>
+    {
+        private readonly string connectionString;
+        
+        public PatientDataAccess(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+        
+        public async Task<Patient> Get(Patient entity)
+        {
+            Patient patient = null;
+            using (var ctx = new SQLiteDatabaseContext(this.connectionString))
+            {
+                patient = (Patient) await ctx.Patients.FindAsync(entity);
+            }
+
+            return patient;
+        }
+
+        public async Task<Patient> GetById(long id)
+        {
+            Patient patient = null;
+            using (var ctx = new SQLiteDatabaseContext(this.connectionString))
+            {
+                patient = (Patient) await ctx.Patients.FindAsync(new Patient{ Id = id});
+            }
+
+            return patient;
+        }
+
+        public async Task Update(Patient entity)
+        {
+          //  using (var ctx = new SQLiteDatabaseContext(this.connectionString))
+              //  await ctx.Patients.Update(entity);
+              throw new NotImplementedException();
+        }
+        
+        public async Task Save(Patient entity)
+        {
+            using (var ctx = new SQLiteDatabaseContext(this.connectionString))
+                await ctx.Patients.AddAsync(entity);
+        }
+    }
+}
