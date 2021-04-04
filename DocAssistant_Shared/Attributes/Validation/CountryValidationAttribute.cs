3ff84faedd67368ba96d70cf8712fbf7190b579c
KDani-99 <porsekin@gmail.com>
2021-04-04 13:27:05 +0200

    Add CountryValidation

---
+++ DocAssistant_Shared/Attributes/Validation/CountryValidationAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+
+namespace DocAssistant_Common.Attributes
+{
+    public class CountryValidationAttribute : ValidationAttribute
+    {
+        private readonly Regex countryRegex = new Regex("^[a-zA-Z ]+$");
+        
+        public override bool IsValid([NotNull] object value)
+        {
+            string country = (string) value;
+
+            return countryRegex.IsMatch(country);
+        }
+    }
+}
