c8f0e349883fc9c66b1f1dfb2975a5e21c3e9ef3
KDani-99 <porsekin@gmail.com>
2021-04-04 21:11:53 +0200

    Add DoctorDataAccess

---
+++ DocAssistantWebApi/Database/DataAccess/DoctorDataAccess.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using DocAssistantWebApi.Database.DbModels;
+
+namespace DocAssistantWebApi.Database.DataAccess
+{
+    public class DoctorDataAccess : IDataAccess<Doctor>
+    {
+        public Task<Doctor> Get(Doctor entity)
+        {
+            Patient patient = null;
+            
+            await using var ctx = new SQLiteDatabaseContext(this.connectionString);
+            
+            patient = (Patient) await ctx.Patients.FindAsync(entity);
+
+            return patient;
+        }
+
+        public Task<Doctor> GetById(long id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Update(Doctor entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Save(Doctor entity)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
