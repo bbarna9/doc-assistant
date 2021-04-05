9376f0bb561d2167950c04ed751cf70b9100993c
KDani-99 <porsekin@gmail.com>
2021-04-05 20:37:05 +0200

    (Test) Add Auth requirement for authorization

---
+++ DocAssistantWebApi/Requirements/DoctorAuthRequirement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using Microsoft.AspNetCore.Authorization;
+
+namespace DocAssistantWebApi.Filters
+{
+    public class DoctorAuthRequirement : AuthorizationHandler<DoctorAuthRequirement> , IAuthorizationRequirement
+    {
+        protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, DoctorAuthRequirement requirement)
+        {
+            
+        }
+        
+    }
+}
