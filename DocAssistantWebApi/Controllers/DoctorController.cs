da201a8eb83953a730b1f3b5eda815c07c3da0e8
KDani-99 <porsekin@gmail.com>
2021-04-05 20:55:02 +0200

    Add DoctorController

---
+++ DocAssistantWebApi/Controllers/DoctorController.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DocAssistant_Common.Models;
+using DocAssistantWebApi.Database.DbModels;
+using DocAssistantWebApi.Database.Repositories;
+using DocAssistantWebApi.Filters;
+using DocAssistantWebApi.Utils;
+using Microsoft.AspNetCore.Authorization;
+
+namespace DocAssistantWebApi.Controllers
+{
+    [ApiController]
+    public class DoctorController : ControllerBase
+    {
+
+        private readonly IRepository<Database.DbModels.Doctor> _repository;
+        
+        public DoctorController(IRepository<Database.DbModels.Doctor> repository)
+        {
+            this._repository = repository;
+        }
+        
+        [Route("/api/doc/create")]
+        [Produces("application/json")]
+        [HttpPost]
+        public async Task<ActionResult> Create()
+        {
+            await _repository.Save(new Database.DbModels.Doctor
+            {
+                FirstName = "TestFirstName",
+                LastName = "TestLastName",
+                Username = "Test",
+                Password = SecurityUtils.CreatePasswordHash("testpassword234")
+            });
+            
+            return Ok();
+        }
+    }
+}
