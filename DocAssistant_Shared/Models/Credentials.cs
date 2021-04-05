7c25af8bbf57cb8c16057aebf82a94c034132244
KDani-99 <porsekin@gmail.com>
2021-04-05 20:35:19 +0200

    Add Credentials class

---
+++ DocAssistant_Shared/Models/Credentials.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DocAssistant_Common.Models
+{
+    public sealed class Credentials
+    {
+        [Required] public string Username { get; set; }
+        [Required] public string Password { get; set; }
+    }
+}
