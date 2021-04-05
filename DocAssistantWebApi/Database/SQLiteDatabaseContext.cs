141b4b97b06360cfdf22ff06d810cdf1917efbef
KDani-99 <porsekin@gmail.com>
2021-04-05 20:54:50 +0200

    Use static connection string instead

--- DocAssistantWebApi/Database/SQLiteDatabaseContext.cs
+++ DocAssistantWebApi/Database/SQLiteDatabaseContext.cs
@@ -1,23 +1,17 @@
 ﻿using System.IO;
 using DocAssistantWebApi.Database.DbModels;
 using Microsoft.EntityFrameworkCore;
-using Patient = DocAssistant_Common.Models.Patient;
 
 namespace DocAssistantWebApi.Database
 {
     public sealed class SQLiteDatabaseContext : DbContext
     {
+        public static string ConnectionString { get; set; }
+
         public DbSet<Doctor> Doctors { get; set; }
         public DbSet<Patient> Patients { get; set; }
 
-        private readonly string connectionString; // @"Data Source=" + Directory.GetCurrentDirectory() + "/healthcare.db"
-        
-        public SQLiteDatabaseContext(string connectionString)
-        {
-            this.connectionString = connectionString;
-        }
-
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlite(this.connectionString); 
+            => options.UseSqlite(ConnectionString); 
     }
 }
