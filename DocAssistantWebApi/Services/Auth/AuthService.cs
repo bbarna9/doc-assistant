d9d8df65d85e01c09af5f6e512b2fef456177980
KDani-99 <porsekin@gmail.com>
2021-04-05 20:41:59 +0200

    Add IAuthService, AuthService DI

--- DocAssistantWebApi/Services/Auth/AuthService.cs
+++ DocAssistantWebApi/Services/Auth/AuthService.cs
@@ -1,24 +1,62 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using DocAssistantWebApi.Database.DbModels;
 using DocAssistantWebApi.Database.Repositories;
 using DocAssistantWebApi.Utils;
+using System.Collections.Generic;
+using System.Security.Policy;
 
-namespace DocAssistantWebApi.Services
+namespace DocAssistantWebApi.Services.Auth
 {
-    public class Session : ISession
+    public class AuthService : IAuthService
     {
+        private readonly Dictionary<long, string> _accessTokens;
+            
         private readonly IRepository<Doctor> _doctorRepository;
         
-        public Session(IRepository<Doctor> doctorRepository)
+        public AuthService(IRepository<Doctor> doctorRepository)
         {
+            this._accessTokens = new Dictionary<long, string>();
             this._doctorRepository = doctorRepository;
         }
-        public async Task<bool> Auth(string username, string password)
+        
+        public async Task<Doctor> Auth(string username, string password)
         {
             Doctor doctor = await this._doctorRepository.Where(
                 doctor => doctor.Username.Equals(username));
 
-            return SecurityUtils.VerifyPassword(password, doctor.Password);
+            if (doctor == null) return null;
+
+            if (!SecurityUtils.VerifyPassword(password, doctor.Password)) return null;
+
+            if (_accessTokens.ContainsKey(doctor.Id)) _accessTokens.Remove(doctor.Id); // log out
+
+            return doctor;
+        }
+
+        public async Task<string> GenerateAccessToken(Doctor doctor)
+        {
+            string token = SecurityUtils.GenerateAccessToken(doctor.Id);
+
+            _accessTokens.Add(doctor.Id,token.Split('.')[1]);
+            
+            return token;
+        }
+
+        public async Task<bool> Authorize(string accessToken)
+        {
+            return SecurityUtils.VerifyAccessToken(_accessTokens, accessToken,out _);
+        }
+        
+        public async Task<bool> Logout(string accessToken)
+        {
+            long docId;
+            
+            if(!SecurityUtils.VerifyAccessToken(_accessTokens, accessToken,out docId)) return false;
+
+            this._accessTokens.Remove(docId);
+
+            return true;
         }
     }
 }
---
+++ DocAssistantWebApi/Services/Auth/IAuthService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using DocAssistantWebApi.Database.DbModels;
+
+namespace DocAssistantWebApi.Services.Auth
+{
+    public interface IAuthService
+    {
+        public Task<Doctor> Auth(string username, string password);
+        public Task<string> GenerateAccessToken(Doctor doctor);
+        public Task<bool> Authorize(string accessToken);
+        public Task<bool> Logout(string accessToken);
+    }
+}
