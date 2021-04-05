a839bca25a34d8a231b9bc8f101fa2fd054d40f4
KDani-99 <porsekin@gmail.com>
2021-04-05 20:36:36 +0200

    Add Person base model class

---
+++ DocAssistant_Shared/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace DocAssistant_Common.Models
+{
+    public abstract class Person
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
