bd8e1cb023bc9ae2b87aee5c69ac6baab553faad
KDani-99 <porsekin@gmail.com>
2021-04-06 19:55:56 +0200

    Add Key attribute to Id

--- DocAssistant_Shared/Models/Person.cs
+++ DocAssistant_Shared/Models/Person.cs
@@ -1,9 +1,16 @@
-﻿namespace DocAssistant_Common.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DocAssistant_Common.Models
 {
     public abstract class Person
     {
-        public long Id { get; set; }
+        [Key] public long Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        public override string ToString()
+        {
+            return $"[Id={Id};FirstName={FirstName};LastName={LastName}]";
+        }
     }
 }
