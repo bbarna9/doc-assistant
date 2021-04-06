6644447c4178173d0c862e9537f2ce71990774c5
KDani-99 <porsekin@gmail.com>
2021-04-06 19:55:42 +0200

    Add doc properties

--- DocAssistant_Shared/Models/Doctor.cs
+++ DocAssistant_Shared/Models/Doctor.cs
@@ -1,7 +1,12 @@
-﻿namespace DocAssistant_Common.Models
+﻿using System.Collections.Generic;
+
+namespace DocAssistant_Common.Models
 {
     public class Doctor : Person
     {
+        public string Username { get; set; }
+        public string Password { get; set; }
         
+        public List<Patient> Patients { get; set; } = new List<Patient>();
     }
 }
