ed02e5167b940d49eee94ca8a58032d38deb8cbb
KDani-99 <porsekin@gmail.com>
2021-04-06 19:57:31 +0200

    Add AddPatient endpoint

--- DocAssistantWebApi/Controllers/PatientController.cs
+++ DocAssistantWebApi/Controllers/PatientController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using DocAssistant_Common.Models;
+using DocAssistantWebApi.Database.Repositories;
 using DocAssistantWebApi.Filters;
+using DocAssistantWebApi.Services.Auth;
 using Microsoft.AspNetCore.Authorization;
 
 namespace DocAssistantWebApi.Controllers
@@ -9,6 +13,18 @@ namespace DocAssistantWebApi.Controllers
     [Route("api/patient")]
     public class PatientController : ControllerBase
     {
+
+        private readonly IRepository<Patient> _patientRepository;
+        private readonly IRepository<Doctor> _doctorRepository;
+        private readonly IAuthService _authService;
+        
+        public PatientController(IRepository<Patient> repository,IRepository<Doctor> doctorRepository,IAuthService authService)
+        {
+            this._patientRepository = repository;
+            this._doctorRepository = doctorRepository;
+            this._authService = authService;
+        }
+        
         [HttpGet]
         public ActionResult<Patient> Get()
         {
@@ -17,12 +33,28 @@ namespace DocAssistantWebApi.Controllers
             );
         }
 
-        [Authorize(Policy = "DoctorAuth")]
+      //  [Authorize(Policy = "DoctorAuth")]
         [Produces("application/json")]
         [HttpPost]
-        public ActionResult Save(/*[FromBody] Patient patient*/)
+        public async Task<ActionResult> AddPatient([FromHeader(Name = "Authorization")] string token,[FromBody] Patient patient)
         {
+            var result = await this._authService.Authorize(token);
+
+            if (!result.Item1)
+            {
+                return Unauthorized();
+            }
+
+            var doctor = await this._doctorRepository.Where(doctor => doctor.Id == result.Item2);
+            doctor.Patients.Add(patient);
+            await this._doctorRepository.Update(doctor);
+
             return Ok();
         }
+
+       /* public async Task<ActionResult> UpdatePatientData()
+        {
+            throw new NotImplementedException();
+        }*/
     }
 }
