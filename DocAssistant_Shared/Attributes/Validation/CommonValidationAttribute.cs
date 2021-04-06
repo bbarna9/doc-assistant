54c6fb1cd6cb658c81b71767f61871f75219d44a
KDani-99 <porsekin@gmail.com>
2021-04-06 19:51:11 +0200

    Use CommonValidationAttribute instead of writing duplicated code

---
+++ DocAssistant_Shared/Attributes/Validation/CommonValidationAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+
+namespace DocAssistant_Common.Attributes
+{
+    public class CommonValidationAttribute : ModelValidationAttribute
+    {
+        public CommonValidationAttribute(string pattern) : base(pattern) {}
+        
+        public override bool IsValid([NotNull] object value)
+        {
+            var val = (string) value;
+
+            return value != null && this.pattern.IsMatch(val);
+        }
+    }
+}
