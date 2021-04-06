29de9869c7916999f5a2313dfd0c3da8e4cd23ea
KDani-99 <porsekin@gmail.com>
2021-04-06 19:54:54 +0200

    Make SSN unique

--- DocAssistantWebApi/Database/SQLiteDatabaseContext.cs
+++ DocAssistantWebApi/Database/SQLiteDatabaseContext.cs
@@ -1,5 +1,4 @@
-﻿using System.IO;
-using DocAssistantWebApi.Database.DbModels;
+﻿using DocAssistant_Common.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace DocAssistantWebApi.Database
@@ -10,6 +9,14 @@ namespace DocAssistantWebApi.Database
 
         public DbSet<Doctor> Doctors { get; set; }
         public DbSet<Patient> Patients { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Patient>(entity => entity.HasIndex(patient => patient.SSN).IsUnique());
+            modelBuilder.Entity<Patient>()
+                .HasIndex(patient => patient.SSN)
+                .IsUnique();
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
             => options.UseSqlite(ConnectionString); 
