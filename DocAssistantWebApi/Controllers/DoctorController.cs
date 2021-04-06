d1df22855c0e7749f6e1fb4c9b25869838bed1fa
KDani-99 <porsekin@gmail.com>
2021-04-06 19:57:57 +0200

    Add Register endpoint

--- DocAssistantWebApi/Controllers/DoctorController.cs
+++ DocAssistantWebApi/Controllers/DoctorController.cs
@@ -1,11 +1,13 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using DocAssistant_Common.Models;
-using DocAssistantWebApi.Database.DbModels;
 using DocAssistantWebApi.Database.Repositories;
-using DocAssistantWebApi.Filters;
+using DocAssistantWebApi.Errors;
+using DocAssistantWebApi.Services.Auth;
 using DocAssistantWebApi.Utils;
-using Microsoft.AspNetCore.Authorization;
+using Newtonsoft.Json.Linq;
 
 namespace DocAssistantWebApi.Controllers
 {
@@ -13,25 +15,45 @@ namespace DocAssistantWebApi.Controllers
     public class DoctorController : ControllerBase
     {
 
-        private readonly IRepository<Database.DbModels.Doctor> _repository;
+        private readonly IRepository<Doctor> _repository;
+        private readonly IAuthService _authService;
         
-        public DoctorController(IRepository<Database.DbModels.Doctor> repository)
+        public DoctorController(IRepository<Doctor> repository,IAuthService authService)
         {
             this._repository = repository;
+            this._authService = authService;
         }
         
-        [Route("/api/doc/create")]
+        [Route("/api/doc/register")]
         [Produces("application/json")]
         [HttpPost]
-        public async Task<ActionResult> Create()
+        public async Task<ActionResult> Register([Microsoft.AspNetCore.Mvc.FromBody] Credentials credentials)
         {
-            await _repository.Save(new Database.DbModels.Doctor
+
+            if (await this._repository.Where(doctor => doctor.Username.Equals(credentials.Username)) != null)
             {
-                FirstName = "TestFirstName",
-                LastName = "TestLastName",
-                Username = "Test",
-                Password = SecurityUtils.CreatePasswordHash("testpassword234")
-            });
+                var errors = new Dictionary<string, string[]>()
+                {
+                    {"Username",new []{"Username is already in use"}}
+                };
+                
+                return BadRequest(
+                    new BadRequestProblemDetails(errors)
+                    {
+                        Title = "Failed to create a new doctor account",
+                        StatusCode = 400
+                    }
+                );
+            }
+
+            var doctor = new Doctor
+            {
+                Username = credentials.Username,
+                Password = SecurityUtils.CreatePasswordHash(credentials.Password),
+                
+            };
+
+            await _repository.Save(doctor);
             
             return Ok();
         }
