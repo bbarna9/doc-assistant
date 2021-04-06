c146303c2f8c802892d9764934aca23838aa33c1
KDani-99 <porsekin@gmail.com>
2021-04-06 19:51:22 +0200

    Test

---
+++ DocAssistantWebApi/Errors/BadRequestProblemDetails.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace DocAssistantWebApi.Errors
+{
+    public class BadRequestProblemDetails
+    {
+        [JsonProperty("status")]
+        public int StatusCode { get; set; }
+        
+        public string Title { get; set; }
+        
+        [JsonPropertyName("errors")]
+        public IDictionary<string, string[]> Errors { get; }
+        
+        public BadRequestProblemDetails(IDictionary<string, string[]> errors)
+        {
+            this.Errors = errors;
+        }
+    }
+}
