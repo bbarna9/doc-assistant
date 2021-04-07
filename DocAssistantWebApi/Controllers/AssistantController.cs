369c2f44be305454756eea27a994505eecb689b7
KDani-99 <porsekin@gmail.com>
2021-04-07 22:12:00 +0200

    Add AssistantController

---
+++ DocAssistantWebApi/Controllers/AssistantController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DocAssistant_Common.Models;
+using DocAssistantWebApi.Database.Repositories;
+using DocAssistantWebApi.Errors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DocAssistantWebApi.Controllers
+{
+    [ApiController]
+    public class AssistantController : ControllerBase
+    {
+        private readonly IRepository<Assistant> _repository;
+        private readonly IRepository<Doctor> _doctorRepository;
+
+        public AssistantController(IRepository<Assistant> repository,IRepository<Doctor> doctorRepository)
+        {
+            this._repository = repository;
+            this._doctorRepository = doctorRepository;
+        }
+    }
+}
