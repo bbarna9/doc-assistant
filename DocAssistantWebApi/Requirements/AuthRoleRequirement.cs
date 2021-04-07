cd041fe2d39a3ae73791f5d321585f2b6e1f8119
KDani-99 <porsekin@gmail.com>
2021-04-07 22:14:54 +0200

    Change allowedRoles visibiliy to private

--- DocAssistantWebApi/Requirements/AuthRoleRequirement.cs
+++ DocAssistantWebApi/Requirements/AuthRoleRequirement.cs
@@ -5,21 +5,22 @@ using System.Threading.Tasks;
 using System.Web.Http.Controllers;
 using System.Web.Http.Filters;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.OpenApi.Extensions;
 
 namespace DocAssistantWebApi.Filters
 {
     public class AuthRoleRequirement : AuthorizationHandler<AuthRoleRequirement> , IAuthorizationRequirement
     {
-        public readonly Roles[] AllowedRoles;
+        private readonly Roles[] _allowedRoles;
         public AuthRoleRequirement(Roles[] allowedRoles)
         {
-            AllowedRoles = allowedRoles;
+            _allowedRoles = allowedRoles;
         }
         protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, AuthRoleRequirement requirement)
         {
-            foreach (var role in AllowedRoles)
+            foreach (var role in _allowedRoles)
             {
-                if (context.User.IsInRole(nameof(role)))
+                if (context.User.IsInRole(role.GetDisplayName()))
                 {
                     context.Succeed(requirement);
                     return;
