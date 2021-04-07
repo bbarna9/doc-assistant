ec6d00b425e3fdbc488d96aae4f88e9631ff98f7
KDani-99 <porsekin@gmail.com>
2021-04-07 22:41:12 +0200

    -

--- DocAssistantWebApi/Startup.cs
+++ DocAssistantWebApi/Startup.cs
@@ -155,7 +155,7 @@ namespace DocAssistantWebApi
             });
             */
 
-            app.UseAuthorization();
+           app.UseAuthorization();
             
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
