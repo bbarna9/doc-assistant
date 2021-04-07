ce1508b1791658984a3fc6c8b3f2cde587d963ad
KDani-99 <porsekin@gmail.com>
2021-04-07 22:09:39 +0200

    Add Assistant class

---
+++ DocAssistant_Shared/Models/Assistant.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DocAssistant_Common.Models
+{
+    public class Assistant : Person
+    {
+        [Required]
+        [ForeignKey("DoctorId")] public long DoctorId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
