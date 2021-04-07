a4a18e1ab7cb4bfd8daedc8afef9136a023a3db7
KDani-99 <porsekin@gmail.com>
2021-04-07 22:17:34 +0200

    Extend auth with assistant/doctor type

--- DocAssistantWebApi/Controllers/AuthController.cs
+++ DocAssistantWebApi/Controllers/AuthController.cs
@@ -2,13 +2,11 @@
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using DocAssistant_Common.Models;
-using DocAssistantWebApi.Database.DbModels;
 using DocAssistantWebApi.Services.Auth;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ActionResult = Microsoft.AspNetCore.Mvc.ActionResult;
 using ControllerBase = Microsoft.AspNetCore.Mvc.ControllerBase;
-using Doctor = DocAssistantWebApi.Database.DbModels.Doctor;
 
 namespace DocAssistantWebApi.Controllers
 {
@@ -16,24 +14,44 @@ namespace DocAssistantWebApi.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
-        private readonly IAuthService _authService;
+        private readonly IAuthService<Doctor> _doctorAuthService;
+        private readonly IAuthService<Assistant> _assistantAuthService;
 
-        public AuthController(IAuthService authService)
+        public AuthController(IAuthService<Doctor> doctorAuthService,IAuthService<Assistant> assistantAuthService)
         {
-            this._authService = authService;
+            this._doctorAuthService = doctorAuthService;
+            this._assistantAuthService = assistantAuthService;
         }
         
         [Microsoft.AspNetCore.Mvc.Route("/api/auth")]
         [Produces("application/json")]
         [Microsoft.AspNetCore.Mvc.HttpPost]
-        public async Task<ActionResult> Auth([FromBody] Credentials credentials)
+        public async Task<ActionResult> Auth([FromQuery(Name = "type")] string type,[FromBody] Credentials credentials)
         {
-            var doc = await this._authService.Auth(credentials.Username, credentials.Password);
-            if (doc == null) return Unauthorized();
+            string accessToken = null;
+            switch (type)
+            {
+                case "doctor":
 
-            var accessToken = await this._authService.GenerateAccessToken(doc);
-
-            return Ok(accessToken);
+                    var doc = await _doctorAuthService.Auth(credentials.Username, credentials.Password);
+                    if (doc == null) return Unauthorized();
+                    
+                    accessToken = await this._doctorAuthService.GenerateAccessToken(doc);
+                    return Ok(accessToken);
+                    
+                    break;
+                case "assistant":
+                    
+                    var assistant = await _doctorAuthService.Auth(credentials.Username, credentials.Password);
+                    if (assistant == null) return Unauthorized();
+                    
+                    accessToken = await this._doctorAuthService.GenerateAccessToken(assistant);
+                    return Ok(accessToken);
+                    
+                    break;
+                default:
+                    return BadRequest();
+            }
         }
 
         [Microsoft.AspNetCore.Mvc.Route("/api/logout")]
@@ -41,7 +59,7 @@ namespace DocAssistantWebApi.Controllers
         [Microsoft.AspNetCore.Mvc.HttpPost]
         public async Task<ActionResult> Logout([FromHeader(Name = "Authorization")] string accessToken)
         {
-            if (!await this._authService.Logout(accessToken)) return Unauthorized();
+            //if (!await this._authService.Logout(accessToken)) return Unauthorized();
             
             return Ok();
         }
