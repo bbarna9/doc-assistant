af24048031ab98451f8edc9f56651479d6aa64f9
KDani-99 <porsekin@gmail.com>
2021-04-07 22:11:35 +0200

    Rename StringExtensions -> Extensions

--- DocAssistantWebApi/Utils/Extensions.cs
+++ DocAssistantWebApi/Utils/Extensions.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using DocAssistantWebApi.Filters;
+using Microsoft.OpenApi.Extensions;
 
 namespace DocAssistantWebApi.Utils
 {
-    public static class StringExtensions
+    public static class Extensions
     {
+        public static IEnumerable<string> GetFormattedRoles(this Roles[] roles) => roles.Select(role => role.GetDisplayName().ToLower());
     }
 }
