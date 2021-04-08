d0baf599474ecd994de313f3c3dc601d37d42a73
KDani-99 <porsekin@gmail.com>
2021-04-08 17:45:56 +0200

    Remove commented code

--- DocAssistantWebApi/Startup.cs
+++ DocAssistantWebApi/Startup.cs
@@ -1,13 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Security.Principal;
-using System.Threading.Tasks;
-using System.Web.Mvc;
 using DocAssistant_Common.Models;
 using DocAssistantWebApi.Database;
 using DocAssistantWebApi.Database.Repositories;
+using DocAssistantWebApi.Errors;
 using DocAssistantWebApi.Filters;
 using DocAssistantWebApi.Services.Auth;
 using DocAssistantWebApi.Services.Auth.AuthHandler;
@@ -15,19 +10,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Routing;
+using Microsoft.Data.Sqlite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Extensions;
 using Microsoft.OpenApi.Models;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
-using Patient = DocAssistant_Common.Models.Patient;
 
 namespace DocAssistantWebApi
 {
@@ -75,15 +62,6 @@ namespace DocAssistantWebApi
                     }))
                 );
             });
-
-           /* services.Configure<IdentityOptions>(options =>
-            {
-                options.Password.RequireDigit = true;
-            });*/
-            
-           // services.AddAuthorization(options =>
-                //options.AddPolicy("RequireDoctorRole", policy => policy.Requirements.Add(new DoctorAuthRequirement()))
-           // );
             
             SQLiteDatabaseContext.ConnectionString = Configuration.GetConnectionString("SQLiteDatabase");
         }
@@ -132,50 +110,8 @@ namespace DocAssistantWebApi
             app.UseRouting();
             
             app.UseAuthentication();
-            
-            
-           /* app.Use(async (context, next) =>
-            {
-                Console.WriteLine("->>"+context.Request.Path);
-                
-               
 
-                var claims = new List<Claim>
-                {
-                    new Claim(ClaimTypes.Role,nameof(Roles.Assistant))
-                };
-                context.User.AddIdentity(new ClaimsIdentity(claims));
-                Console.WriteLine(context.User.IsInRole(nameof(Roles.Assistant)));
-                
-                var serializerSettings = new JsonSerializerSettings();
-                serializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-                
-                Console.WriteLine(JsonConvert.SerializeObject(new
-                {
-                    Roles = new Roles [] {Roles.Assistant}
-                },serializerSettings));
-                
-                //
-                context.Items.Add("Id",1);
-                //
-                
-                //context.GetRouteData().Routers.ToList().ForEach(elem => Console.WriteLine(elem.GetType()));
-               
-            /*    var cultureQuery = context.Request.Query["culture"];
-                if (!string.IsNullOrWhiteSpace(cultureQuery))
-                {
-                    var culture = new CultureInfo(cultureQuery);
-
-                    CultureInfo.CurrentCulture = culture;
-                    CultureInfo.CurrentUICulture = culture;
-                }
-
-                
-                await next();
-            });
-            */
-
-           app.UseAuthorization();
+            app.UseAuthorization();
             
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
