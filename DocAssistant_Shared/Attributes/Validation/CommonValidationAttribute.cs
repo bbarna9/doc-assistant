572a0151b9d4c3958e393b6450386874eeb237c9
KDani-99 <porsekin@gmail.com>
2021-04-08 17:46:31 +0200

    Add AttributeUsage to custom attribute

--- DocAssistant_Shared/Attributes/Validation/CommonValidationAttribute.cs
+++ DocAssistant_Shared/Attributes/Validation/CommonValidationAttribute.cs
@@ -1,9 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
 using System.Text.RegularExpressions;
 
 namespace DocAssistant_Common.Attributes
 {
+    [AttributeUsage(AttributeTargets.Property,AllowMultiple = true, Inherited = true)]
     public class CommonValidationAttribute : ModelValidationAttribute
     {
         public CommonValidationAttribute(string pattern) : base(pattern) {}
