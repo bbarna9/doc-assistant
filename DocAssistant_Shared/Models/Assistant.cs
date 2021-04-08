837b3ccdf0f09c282e63b421ee8a494d78a31f46
KDani-99 <porsekin@gmail.com>
2021-04-08 17:49:36 +0200

    Derive Assistant from Staff

--- DocAssistant_Shared/Models/Assistant.cs
+++ DocAssistant_Shared/Models/Assistant.cs
@@ -1,13 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using DocAssistant_Common.Attributes;
 
 namespace DocAssistant_Common.Models
 {
-    public class Assistant : Person
+    public class Assistant : Staff
     {
+        [Fixed]
         [Required]
         [ForeignKey("DoctorId")] public long DoctorId { get; set; }
-        [Required] public string Username { get; set; }
-        [Required] public string Password { get; set; }
     }
 }
