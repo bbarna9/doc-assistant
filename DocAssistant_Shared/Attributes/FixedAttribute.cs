d5667d921576696273def1a96a23a275c39c98f0
KDani-99 <porsekin@gmail.com>
2021-04-08 17:46:47 +0200

    Add FixedAttribute to mark constant properties

---
+++ DocAssistant_Shared/Attributes/FixedAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DocAssistant_Common.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property,AllowMultiple = true, Inherited = true)]
+    public class FixedAttribute : Attribute
+    {
+        // Marks a property as not modifiable
+    }
+}
