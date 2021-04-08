d195c663dbb6783a662694eec682f13115dfd712
KDani-99 <porsekin@gmail.com>
2021-04-08 17:48:01 +0200

    Add Staff base class

---
+++ DocAssistant_Shared/Models/Staff.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using DocAssistant_Common.Attributes;
+
+namespace DocAssistant_Common.Models
+{
+    public abstract class Staff : Person
+    {
+        public virtual string Username { get; set; }
+        public virtual string Password { get; set; }
+    }
+}
