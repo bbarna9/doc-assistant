9d212f66eb2b968e4bb12684baa4639f1ecede68
KDani-99 <porsekin@gmail.com>
2021-04-08 17:50:50 +0200

    Add new interface methods

--- DocAssistantWebApi/Services/Auth/IAuthService.cs
+++ DocAssistantWebApi/Services/Auth/IAuthService.cs
@@ -9,11 +9,10 @@ namespace DocAssistantWebApi.Services.Auth
     public interface IAuthService<T>
     {
         public Task<T> Auth(string username, string password);
-        public Task<string> GenerateAccessToken(T doctor);
+        public Task<string> GenerateAccessToken(T entity);
         public (bool, long, IEnumerable<Roles>) VerifyAccessToken(string token);
-      //  public Task<string> GenerateAccessToken(Assistant assistant);
-        public Task<(bool, long)> Authorize(string accessToken);
-        public Task<bool> Logout(string accessToken);
+      //  public Task<string> GenerateAccessToken(Assistant assistant););
+        public Task<bool> Logout(long id);
 
         public static Roles GetRoleByName(string name)
         {
