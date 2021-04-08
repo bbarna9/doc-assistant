d89228d7a1996ddda3acb08c900f00d9ebd4528b
KDani-99 <porsekin@gmail.com>
2021-04-08 17:44:48 +0200

    Replace BadRequestProblemDetails with GenericRequestError

---
+++ DocAssistantWebApi/Errors/GenericRequestError.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace DocAssistantWebApi.Errors
+{
+    public class GenericRequestException : Exception
+    {
+        [JsonProperty("status")] public int StatusCode { get; set; }
+        [JsonProperty("title")] public string Title { get; set; }
+        [JsonProperty("error")] public string Error { get; set; }
+        
+        public GenericRequestException() {}
+
+        public GenericRequestException(int statusCode, string title, string error)
+        {
+            this.StatusCode = statusCode;
+            this.Title = title;
+            this.Error = error;
+        }
+    }
+}
