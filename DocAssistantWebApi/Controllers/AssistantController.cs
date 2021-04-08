0e8e6af6db461b048d7d9ac0472391e7b18a7221
KDani-99 <porsekin@gmail.com>
2021-04-08 17:57:39 +0200

    Move RegisterAssistant endpoint from DoctorController

--- DocAssistantWebApi/Controllers/AssistantController.cs
+++ DocAssistantWebApi/Controllers/AssistantController.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using DocAssistant_Common.Models;
 using DocAssistantWebApi.Database.Repositories;
 using DocAssistantWebApi.Errors;
+using DocAssistantWebApi.Filters;
+using DocAssistantWebApi.Utils;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DocAssistantWebApi.Controllers
@@ -10,13 +12,104 @@ namespace DocAssistantWebApi.Controllers
     [ApiController]
     public class AssistantController : ControllerBase
     {
-        private readonly IRepository<Assistant> _repository;
+        private readonly IRepository<Assistant> _assistantRepository;
         private readonly IRepository<Doctor> _doctorRepository;
 
-        public AssistantController(IRepository<Assistant> repository,IRepository<Doctor> doctorRepository)
+        public AssistantController(IRepository<Assistant> assistantRepository,IRepository<Doctor> doctorRepository)
         {
-            this._repository = repository;
+            this._assistantRepository = assistantRepository;
             this._doctorRepository = doctorRepository;
+        }
+        
+        [Authorize(Policy = "DoctorRequirement")]
+        [Route("/api/assistant/")]
+        [Produces("application/json")]
+        [HttpPost]
+        public async Task<ActionResult> RegisterAssistant([FromBody] Credentials data)
+        {
+            if (await this._assistantRepository.Where(assistant => assistant.Username.Equals(data.Username)) != null)
+            {
+                throw new GenericRequestException
+                {
+                    Title = "Failed to create a new assistant account",
+                    Error =
+                        "Username is already in use",
+                    StatusCode = 400
+                };
+            }
+
+            var doctorId = (long) HttpContext.Items["Id"];
+
+            var assistant = new Assistant
+            {
+                Username = data.Username,
+                Password = SecurityUtils.CreatePasswordHash(data.Password),
+                DoctorId = doctorId
+            };
+            
+            var doctor = await this._doctorRepository.Where(doctor => doctor.Id == doctorId);
+            doctor.Assistants.Add(assistant);
+            
+            if(!await this._doctorRepository.Update(doctor))
+                throw new GenericRequestException
+                {
+                    Title = "Failed to create a new assistant account",
+                    Error = "Save failed",
+                    StatusCode = 400
+                };
+
+            return Ok();
+        }
+        
+        [Authorize(Policy = "AssistantRequirement")]
+        [Route("/api/assistant/")]
+        [Produces("application/json")]
+        [HttpPatch]
+        public async Task<ActionResult> UpdateAssistantData([FromBody] Assistant data)
+        {
+
+            Roles accountType = (Roles) HttpContext.Items["AccountType"];
+            long assistantId;
+
+            if (accountType == Roles.Doctor)
+            {
+                // Doctors can update assistant data
+                long doctorId = (long) HttpContext.Items["Id"];
+
+                var assistant = await this._assistantRepository.Where(assistant => assistant.Id == data.Id);
+                if(assistant == null)
+                    throw new GenericRequestException
+                    {
+                        Title = "Failed to update data",
+                        Error = "Assistant does not exist",
+                        StatusCode = 400
+                    };
+                
+                if(assistant.DoctorId != doctorId)
+                    throw new GenericRequestException
+                    {
+                        Title = "Failed to update data",
+                        Error = "Assistant works for a different doctor",
+                        StatusCode = 400
+                    };
+                
+                assistantId = assistant.Id;
+            }
+            else 
+                assistantId = (long) HttpContext.Items["Id"];
+
+            data.Id = assistantId;
+
+            if(!await this._assistantRepository.UpdateChangedProperties(data))
+                throw new GenericRequestException
+                {
+                    Title = "Failed to update data",
+                    Error =
+                        "No changes were made",
+                    StatusCode = 400
+                };
+            
+            return Ok();
         }
     }
 }
