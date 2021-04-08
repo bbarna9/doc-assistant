0958f59677c5977d804845664b166c45dba6debd
KDani-99 <porsekin@gmail.com>
2021-04-08 17:55:34 +0200

    Using reflection

--- DocAssistantWebApi/Database/Repositories/PatientRepository.cs
+++ DocAssistantWebApi/Database/Repositories/PatientRepository.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Threading.Tasks;
 using DocAssistant_Common.Models;
 using Microsoft.EntityFrameworkCore;
