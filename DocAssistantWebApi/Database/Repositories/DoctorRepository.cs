f208ef47a810274044ea2338b4bc7d86dc85a6fa
KDani-99 <porsekin@gmail.com>
2021-04-09 08:17:26 +0200

    Derive repository from BaseRepository

--- DocAssistantWebApi/Database/Repositories/DoctorRepository.cs
+++ DocAssistantWebApi/Database/Repositories/DoctorRepository.cs
@@ -6,75 +6,14 @@ using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using DocAssistant_Common.Models;
+using DocAssistantWebApi.Database.Factories;
 using DocAssistantWebApi.Errors;
 using Microsoft.Data.Sqlite;
 
 namespace DocAssistantWebApi.Database.Repositories
 {
-    public class DoctorRepository : IRepository<Doctor>
+    public class DoctorRepository : BaseRepository<Doctor>
     {
-        public async Task<Doctor> Get(Doctor entity)
-        {
-            Doctor doctor = null;
-            
-            await using var ctx = new SQLiteDatabaseContext();
-            
-            doctor = (Doctor) await ctx.Doctors.FindAsync(entity);
-            
-            return doctor;
-        }
-
-        public async Task<Doctor> Where(Expression<Func<Doctor,bool>> expression)
-        {
-            await using var ctx = new SQLiteDatabaseContext();
-
-            return await ctx.Doctors.FirstOrDefaultAsync(expression);
-        }
-
-        public async Task<bool> UpdateChangedProperties(Doctor entity)
-        {
-            await using var ctx = new SQLiteDatabaseContext();
-            
-            var doctor = await ctx.Doctors.FirstOrDefaultAsync(doctor => doctor.Id == entity.Id);
-
-            var updatedProperties = IRepository<Doctor>.GetUpdatedProperties(doctor,entity);
-
-            foreach (var property in updatedProperties)
-            {
-                doctor.GetType().GetProperty(property.Item1, BindingFlags.Instance | BindingFlags.Public | BindingFlags.IgnoreCase | BindingFlags.NonPublic)
-                    ?.SetValue(doctor,property.Item2);
-            }
-
-            return await ctx.SaveChangesAsync() > 0;
-        }
-
-        public async Task<bool> Update(Doctor entity)
-        {
-
-            await using var ctx = new SQLiteDatabaseContext();
-        
-            ctx.Update(entity);
-
-            return await ctx.SaveChangesAsync() > 0;
-        }
-
-        public async Task Save(Doctor entity)
-        {
-            await using var ctx = new SQLiteDatabaseContext();
-
-            await ctx.AddAsync(entity);
-            await ctx.SaveChangesAsync();
-        }
-        public async Task<IEnumerable<Doctor>> WhereMulti(Expression<Func<Doctor, bool>> expression)
-        {
-            await using var ctx = new SQLiteDatabaseContext();
-
-            return await ctx.Doctors.Where(expression).ToListAsync();
-        }
-
-        public Task<int> DeleteWhere(Expression<Func<Doctor, bool>> expression)
-        {
-            throw new NotImplementedException();
-        }
+        public DoctorRepository(IDatabaseFactory databaseFactory) : base(databaseFactory) {}
     }
 }
--- DocAssistantWebApi/Database/Repositories/PatientRepository.cs
+++ DocAssistantWebApi/Database/Repositories/PatientRepository.cs
@@ -7,89 +7,32 @@ using System.Linq.Expressions;
 using System.Reflection;
 using System.Threading.Tasks;
 using DocAssistant_Common.Models;
+using DocAssistantWebApi.Database.Factories;
 using Microsoft.EntityFrameworkCore;
 
 namespace DocAssistantWebApi.Database.Repositories
 {
-    public class PatientRepository : IRepository<Patient>
+    public class PatientRepository : BaseRepository<Patient>
     {
-        public async Task<Patient> Get(Patient entity)
+        public PatientRepository(IDatabaseFactory databaseFactory) : base(databaseFactory) {}
+
+        public override async Task Save(Patient entity)
         {
-            Patient patient = null;
-            
-            await using var ctx = new SQLiteDatabaseContext();
-            
-            patient = await ctx.Patients.FindAsync(entity);
-
-            return patient;
-        }
-
-        public Task<bool> UpdateChangedProperties(Patient entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public async Task<bool> Update(Patient entity)
-        {
-            await using var ctx = new SQLiteDatabaseContext();
-            
-            var patientToUpdate = await ctx.Patients.FirstOrDefaultAsync(patient => patient.Id == entity.Id);
-            if (patientToUpdate == null) return false;
-            
-            var updatedProperties = IRepository<Patient>.GetUpdatedProperties(patientToUpdate,entity);
-
-            foreach (var property in updatedProperties)
-            {
-                patientToUpdate.GetType().GetProperty(property.Item1, BindingFlags.Instance | BindingFlags.Public | BindingFlags.IgnoreCase | BindingFlags.NonPublic)
-                    ?.SetValue(patientToUpdate,property.Item2);
-            }
-
-            return await ctx.SaveChangesAsync() > 0;
-        }
-        
-        public async Task Save(Patient entity)
-        {
-            await using var ctx = new SQLiteDatabaseContext();
+            await using var ctx = this.DatabaseFactory.Create();
 
             entity.ArriveTime = DateTime.Now;
             
-            await ctx.AddAsync(entity);
-            await ctx.SaveChangesAsync();
+            await ctx.GetContext().AddAsync(entity);
+            await ctx.GetContext().SaveChangesAsync();
         }
 
-        public Task Patch(Patient entity)
+        public override async Task<IEnumerable<Patient>> WhereMulti(Expression<Func<Patient, bool>> expression)
         {
-            throw new NotImplementedException();
-        }
+            await using var ctx = this.DatabaseFactory.Create();
 
-        public async Task<Patient> Where(Expression<Func<Patient,bool>> expression)
-        {
-            await using var ctx = new SQLiteDatabaseContext();
-
-            return await ctx.Patients.FirstOrDefaultAsync(expression);
-        }
-
-        public async Task<IEnumerable<Patient>> WhereMulti(Expression<Func<Patient, bool>> expression)
-        {
-            await using var ctx = new SQLiteDatabaseContext();
-
-            return await ctx.Patients.Where(expression)
+            return await ctx.GetSet<Patient>().Where(expression)
                 .OrderBy(patient => patient.ArriveTime)
                 .ToListAsync();
-        }
-
-        public async Task<int> DeleteWhere(Expression<Func<Patient, bool>> expression)
-        {
-            await using var ctx = new SQLiteDatabaseContext();
-
-            var patient = await ctx.Patients.FirstOrDefaultAsync(expression);
-
-            if (patient == default) return 0;
-            
-            ctx.Patients.Remove(patient);
-            await ctx.SaveChangesAsync();
-
-            return 1;
         }
     }
 }
