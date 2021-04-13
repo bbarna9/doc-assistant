be0940ee864beede53b707e6fd9a5db270bad672
KDani-99 <porsekin@gmail.com>
2021-04-13 09:24:46 +0200

    Derive from BaseRepository

--- DocAssistantWebApi/Database/Repositories/AssistantRepository.cs
+++ DocAssistantWebApi/Database/Repositories/AssistantRepository.cs
@@ -5,13 +5,18 @@ using System.Threading.Tasks;
 using DocAssistant_Common.Models;
 using System.Linq;
 using System.Reflection;
+using DocAssistantWebApi.Database.Factories;
 using Microsoft.EntityFrameworkCore;
+using AssistantModel = DocAssistant_Common.Models.Assistant;
 
-namespace DocAssistantWebApi.Database.Repositories
+namespace DocAssistantWebApi.Database.Repositories.Assistant
 {
-    public class AssistantRepository : IRepository<Assistant>
+    public class AssistantRepository : BaseRepository<AssistantModel>,IAssistantRepository<AssistantModel>
     {
-        public async Task<bool> Update(Assistant entity)
+        public AssistantRepository(IDatabaseFactory databaseFactory) : base(databaseFactory)
+        {
+        }
+        public override async Task<bool> Update(AssistantModel entity)
         {
             await using var ctx = new SQLiteDatabaseContext();
             
@@ -19,56 +24,5 @@ namespace DocAssistantWebApi.Database.Repositories
             
             return await ctx.SaveChangesAsync() > 0;
         }
-        
-        public async Task<bool> UpdateChangedProperties(Assistant entity)
-        {
-            await using var ctx = new SQLiteDatabaseContext();
-            
-            var assistant = await ctx.Assistants.FirstOrDefaultAsync(assistant => assistant.Id == entity.Id);
-
-            #region  Not changeable properties
-            entity.DoctorId = assistant.DoctorId;
-            entity.Id = assistant.Id;
-            #endregion
-
-            var updatedProperties = IRepository<Assistant>.GetUpdatedProperties(assistant,entity);
-
-            foreach (var property in updatedProperties)
-            {
-                assistant.GetType().GetProperty(property.Item1, BindingFlags.Instance | BindingFlags.Public | BindingFlags.IgnoreCase | BindingFlags.NonPublic)
-                    ?.SetValue(assistant,property.Item2);
-            }
-
-            return await ctx.SaveChangesAsync() > 0;
-        }
-        
-        public async Task<Assistant> Where(Expression<Func<Assistant, bool>> expression)
-        {
-            await using var ctx = new SQLiteDatabaseContext();
-
-            return await ctx.Assistants.FirstOrDefaultAsync(expression);
-        }
-        
-        #region Not required
-        public Task<Assistant> Get(Assistant entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task Save(Assistant entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<int> DeleteWhere(Expression<Func<Assistant, bool>> expression)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<Assistant>> WhereMulti(Expression<Func<Assistant, bool>> expression)
-        {
-            throw new NotImplementedException();
-        }
-        #endregion
     }
 }
