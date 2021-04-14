6932e98f1d1df0b4b4d9dddbc796b368a9db1075
KDani-99 <porsekin@gmail.com>
2021-04-14 15:27:52 +0200

    Add Diagnosis class

---
+++ DocAssistant_Shared/Models/Diagnosis.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using DocAssistant_Common.Attributes;
+
+namespace DocAssistant_Common.Models
+{
+    public sealed class Diagnosis
+    {
+        [Fixed] [ForeignKey("PatientId")] public long PatientId { get; set; }
+        [Required] [CommonValidation("^.{1,35}$")] public string Title { get; set; }
+        [Required] [CommonValidation("^.{1,500}$")] public string Description { get; set; }
+        [Required] public DateTime Date { get; set; }
+    }
+}
