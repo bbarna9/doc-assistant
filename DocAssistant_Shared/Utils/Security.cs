d819fd8869e55bf9df14ba87eaf574987277c988
KDani-99 <porsekin@gmail.com>
2021-05-23 17:43:38 +0200

    Add Security utility class

---
+++ DocAssistant_Shared/Utils/Security.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using DocAssistant_Common.Models;
+
+namespace DocAssistant_Common.Utils
+{
+    public static class Security
+    {
+        public static byte[] ComputeHash(string str)
+        {
+            using var sha = new SHA1Managed();
+
+            return sha.ComputeHash(Encoding.ASCII.GetBytes(str));
+            
+        }
+    }
+}
