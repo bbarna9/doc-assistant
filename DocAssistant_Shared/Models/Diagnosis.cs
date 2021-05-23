dff3da5671c4fd48cd00ba30841c32cc035749fa
KDani-99 <porsekin@gmail.com>
2021-05-23 17:42:36 +0200

    Add diagnosis title

--- DocAssistant_Shared/Models/Diagnosis.cs
+++ DocAssistant_Shared/Models/Diagnosis.cs
@@ -9,6 +9,7 @@ namespace DocAssistant_Common.Models
     {
         [Key] public int Id { get; set; }
         [Fixed] [ForeignKey("PatientId")] public long PatientId { get; set; }
+        [Required] [CommonValidation("^.{1,50}$")] public string Title { get; set; }
         [Required] [CommonValidation("^.{1,500}$")] public string Description { get; set; }
         [Required] public DateTime Date { get; set; }
 
